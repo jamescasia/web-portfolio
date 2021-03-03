6f0ca52d9b95300c9a7f7b0e54d35465650ca072
jamescasia <wayperwayp@gmail.com>
2021-03-03 17:06:46 +0800

    added scroll-bar

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -7,6 +7,7 @@ import './PageTwo.dart';
 import './PageThree.dart';
 import './PageFour.dart';
 import './PageFive.dart';
+import 'package:flutter_web_scrollbar/flutter_web_scrollbar.dart';
 
 class PortfolioApp extends StatelessWidget {
   @override
@@ -25,37 +26,67 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  ScrollController _controller;
+  @override
+  void initState() {
+    //Initialize the  scrollController
+    _controller = ScrollController();
+    super.initState();
+  }
+
+  void scrollCallBack(DragUpdateDetails dragUpdate) {
+    setState(() {
+      // Note: 3.5 represents the theoretical height of all my scrollable content. This number will vary for you.
+      _controller.position.moveTo(dragUpdate.globalPosition.dy * 3.5);
+    });
+  }
+
   double safePadding = 0;
   @override
-  Widget build(BuildContext context) { 
+  Widget build(BuildContext context) {
     return SafeArea(child: LayoutBuilder(builder: (context, constraints) {
       Globals.height = MediaQuery.of(context).size.height;
       safePadding = Globals.height - constraints.maxHeight;
-       Globals.width = constraints.maxWidth;
-        Globals.height = constraints.maxHeight;
+      Globals.width = constraints.maxWidth;
+      Globals.height = constraints.maxHeight;
 
-        Globals.dheight = Globals.height / 981;
-        Globals.dwidth = Globals.width / 1873;
+      Globals.dheight = Globals.height / 981;
+      Globals.dwidth = Globals.width / 1873;
       print(Globals.height);
       print(Globals.width);
       return Material(
         child: Scaffold(
-            body: Container(
-          width: Globals.width, 
-          color: Globals.black,
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                PageOne(),
-                PageTwo(),
-                PageThree(),
-                PageFour(),
-                PageFive()
-              ],
+            body: Stack(children: [
+          Container(
+            width: Globals.width,
+            color: Globals.black,
+            child: SingleChildScrollView(
+              controller: _controller,
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  PageOne(),
+                  PageTwo(),
+                  PageThree(),
+                  PageFour(),
+                  PageFive()
+                ],
+              ),
             ),
           ),
-        )),
+          FlutterWebScroller(
+            //Pass a reference to the ScrollCallBack function into the scrollbar
+            scrollCallBack,
+
+            //Add optional values
+            scrollBarBackgroundColor: Colors.white,
+            scrollBarWidth: 20.0,
+            dragHandleColor: Colors.grey,
+            dragHandleBorderRadius: 2.0,
+            dragHandleHeight: 40.0,
+            dragHandleWidth: 15.0,
+          ),
+        ])),
       );
     }));
   }
